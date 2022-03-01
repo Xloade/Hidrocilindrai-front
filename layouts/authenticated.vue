33d3acd46e6f86b2fe026b6807554c465c9eac1c
x-loade <dainius1999@hotmail.lt>
2022-03-01 17:50:28 +0200

    bad links

--- layouts/authenticated.vue
+++ layouts/authenticated.vue
@@ -10,8 +10,8 @@
 
                 <b-collapse id="nav-collapse" is-nav>
                 <b-navbar-nav>
-                    <b-nav-item href="/dashboard">Dashboard</b-nav-item>
-                    <b-nav-item href="/cylinders">My Cylinders</b-nav-item>
+                    <b-nav-item to="/dashboard">Dashboard</b-nav-item>
+                    <b-nav-item to="/cylinders">My Cylinders</b-nav-item>
                 </b-navbar-nav>
 
                 <!-- Right aligned nav items -->
