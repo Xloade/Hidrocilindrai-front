e0c3200a9a67bdb9465eb718ca78ee3832f8cbe7
x-loade <dainius1999@hotmail.lt>
2022-04-15 20:47:46 +0300

    user list

---
+++ pages/users/index.vue
@@ -0,0 +1,105 @@
+<template>
+  <BreezeAuthenticatedLayout>
+    <template #header>
+    <div class="d-flex">
+        <MyHeader :name="title" />
+        <b-button variant="success" v-b-modal.modal-prevent-closing>Add</b-button>
+    </div>
+    </template>
+        <div class="">
+          <my-alert ref="alert"/>
+          <div class="row">
+              <b-table
+                :items="users"
+                :fields="fields"
+              >
+                <template #cell(roles)="{ item }">
+
+                  <template v-if="item.roles[0]">
+                   {{ item.roles[0].name }}
+                  </template>
+                </template>
+
+                <template #cell(actions)="{ item }">
+                  <b-button variant="info" size="sm" @click="edit(item.id)">Edit</b-button>
+                  <b-button variant="danger" size="sm" @click="remove(item.id)">Remove</b-button>
+                </template>
+              </b-table>
+          </div>
+        </div>
+  </BreezeAuthenticatedLayout>
+</template>
+
+<script>
+import BreezeAuthenticatedLayout from '@/layouts/authenticated.vue'
+import MyHeader from '@/components/header.vue'
+import myAlert from '~/components/myAlert.vue'
+export default {
+    data() {
+      return {
+        title: 'Users',
+        users: [],
+        fields: [
+          {
+            key: 'name',
+            label: 'Name',
+          },
+          {
+            key: 'email',
+            label: 'E-mail',
+          },
+          {
+            key: 'roles',
+            label: 'Role',
+          },
+          {
+            key: 'actions',
+            label: 'Actions'
+          }
+        ],
+      }
+    },
+    head() {
+        return{
+            title: this.title,
+        }
+    },
+    asyncData ({ $axios }, callback) {
+        $axios.get('/api/admin/user')
+        .then((res) => {
+            callback(null, { users: res.data })
+        })
+    },
+    methods:{
+      add(){
+
+      },
+      edit(id){
+
+      },
+      remove(id){
+          this.$axios.delete("/api/admin/user/"+id)
+          .then((message) => {
+              this.$refs.alert.setAlert(message.data.message, "success")
+              this.$nuxt.refresh()
+          })
+          .catch((error) => {
+              if( error.response.data.message ){
+                  this.$refs.alert.setAlert(error.response.data.message, "danger")
+              }
+              else{
+                  this.$refs.alert.setAlert(error.message, "danger")
+              }
+          })
+      }
+    },
+
+    middleware: 'authenticated',
+
+    components: {
+        BreezeAuthenticatedLayout,
+        MyHeader,
+        myAlert
+    }
+}
+</script>
